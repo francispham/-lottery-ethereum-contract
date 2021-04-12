ee4c01d9f9a076bb266e5023e90253cdb45acb19
Francis Pham <francispham89@gmail.com>
2021-04-12 15:08:31 -0700

    Implemented Set Appropriate Ether Amount to Enter Lottery Test with Try-Catch Assertions

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -34,4 +34,40 @@ describe('Lottery Contract', () => {
     assert.strictEqual(accounts[0], players[0]);
     assert.strictEqual(1, players.length);
   });
+
+  it('allows Multiple Accounts to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.strictEqual(accounts[0], players[0]);
+    assert.strictEqual(accounts[1], players[1]);
+    assert.strictEqual(accounts[2], players[2]);
+    assert.strictEqual(3, players.length);
+  });
+
+  it('requires a Minimum Amount of Ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0  // This Function has to return an Error because of Zero!
+      });
+      assert(false); // This will make the Test failed if the above Function doesn't throw an Error! 
+    } catch (err) {
+      assert(err);
+    }
+  });
 });
