13c41ab03fd634dfbb065b9385eb5539e8c12bcc
Francis Pham <francispham89@gmail.com>
2021-04-12 15:29:19 -0700

    Implemented End to End Test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -82,4 +82,18 @@ describe('Lottery Contract', () => {
       assert(err);
     }
   });
+
+  it('sends Money to the Winner and resets the Players Array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth. getBalance(accounts[0]);
+
+    const difference = finalBalance - initialBalance;
+    assert(difference > web3.utils.toWei('1.9', 'ether')); // Because of the Gas expensed!
+  });
 });
