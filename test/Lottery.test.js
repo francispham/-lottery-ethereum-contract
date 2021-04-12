cd3715868b90e1eea7e33444d49feb49e4a841fa
Francis Pham <francispham89@gmail.com>
2021-04-12 14:23:16 -0700

    Implemented One Account Entering the Lottery Test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -20,4 +20,18 @@ describe('Lottery Contract', () => {
   it('deployed a Contract', () => {
     assert.ok(lottery.options.address);
   });
+
+  it('allows One Account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.strictEqual(accounts[0], players[0]);
+    assert.strictEqual(1, players.length);
+  });
 });
